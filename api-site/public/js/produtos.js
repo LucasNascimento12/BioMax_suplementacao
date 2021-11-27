a58e7b3c3903c6a3df01f0e945f12fca6432493a
LucasNascimento12 <lucas.nascimento@bandtec.com.br>
2021-11-27 15:34:11 -0300

    html e css

---
+++ api-site/public/js/produtos.js
@@ -0,0 +1,100 @@
+/* growth */
+function whey_growth1() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.gsuplementos.com.br/whey-protein-concentrado-1kg-growth-supplements-p985936"
+}
+
+function multi_growht1() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.gsuplementos.com.br/multivitaminico-120-caps-nova-formula-growth-supplements-p985851"
+}
+
+function pasta_growht1() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.gsuplementos.com.br/pasta-de-amendoim-integral-torrado-1kg-growth-supplements-p988045?busca=pas"
+}
+
+function ome_growth1() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.gsuplementos.com.br/oleo-de-peixe-ultra-75-softgel-growth-supplements-p987568?busca=omega"
+}
+
+/* max titanium  */
+
+
+function whey_max() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.maxtitanium.com.br/-100--whey-refil-900g/p"
+}
+
+function multi_max() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.maxtitanium.com.br/multimax-complex-pote-com-90-capsulas/p"
+}
+
+function ome_max() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.maxtitanium.com.br/omega-3-90-capsulas/p"
+}
+
+
+/* Oficial farma  */
+
+
+function ZMA_oficial() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.oficialfarma.com.br/zma-turbo-90-capsulas/p"
+}
+
+function Arginina_oficial(){
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.oficialfarma.com.br/l-arginina-aspartato-500mg-100-capsulas/p"
+}
+
+function Omega_oficial() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.oficialfarma.com.br/omega-1g-dha-50---ultra----60-capsulas/p"
+}
+
+
+/* Integralmedica */
+
+function whey_integral() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.integralmedica.com.br/whey-100-pure/p"
+}
+
+function ome_integral() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.integralmedica.com.br/omega-3-integralmedica/p"
+}
+
+function multi_integral() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.integralmedica.com.br/reforz-c-immunity/p"
+}
+
+function pasta_integral() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.integralmedica.com.br/pasta%20de%20amendoin"
+}
+
+
+/* Pura vida  */
+
+
+function whey_pura() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.puravida.com.br/whey-protein-grassfed-70213"
+}
+
+function multi_pura() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.puravida.com.br/alpha-men-70280"
+}
+
+function omega_pura() {
+    alert ('produto cadastrado com sucesso')
+   window.location = "https://www.puravida.com.br/omega-3-dha-oleo-de-peixe-70111"
+}
+
